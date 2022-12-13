535a8ef071d81b6abd1a1545420444be7786ab71
Pedro Guilherme Faria Duarte <Pedro.Duarte@011brasil.local>
2022-12-13 14:33:45 -0300

    Improvements to legacy applications, fix critical bug on parse from response body

--- src/App/Models/Transaction/Transaction.php
+++ src/App/Models/Transaction/Transaction.php
@@ -41,76 +41,12 @@ class Transaction implements Parser
     public function pay($transaction = [])
     {
         try {
-            // $fakePaymentData = [
-            //     "token_account" => "SEU_TOKEN_AQUI",
-            //     "customer" => [
-            //         "contacts" => [
-            //             [
-            //                 "type_contact" => "H",
-            //                 "number_contact" => "1133221122"
-            //             ],
-            //             [
-            //                 "type_contact" => "M",
-            //                 "number_contact" => "11999999999"
-            //             ]
-            //         ],
-            //         "addresses" => [
-            //             [
-            //                 "type_address" => "B",
-            //                 "postal_code" => "17000-000",
-            //                 "street" => "Av Esmeralda",
-            //                 "number" => "1001",
-            //                 "completion" => "A",
-            //                 "neighborhood" => "Jd Esmeralda",
-            //                 "city" => "Marilia",
-            //                 "state" => "SP"
-            //             ]
-            //         ],
-            //         "name" => "Stephen Strange",
-            //         "birth_date" => "21/05/1941",
-            //         "cpf" => "50235335142",
-            //         "email" => "stephen.strange@avengers.com"
-            //     ],
-            //     "transaction_product" => [
-            //         [
-            //             "description" => "Camiseta Tony Stark",
-            //             "quantity" => "1",
-            //             "price_unit" => "130.00",
-            //             "code" => "1",
-            //             "sku_code" => "0001",
-            //             "extra" => "Informação Extra"
-            //         ]
-            //     ],
-            //     "transaction" => [
-            //         "available_payment_methods" => "2,3,4,5,6,7,14,15,16,18,19,21,22,23",
-            //         "customer_ip" => "127.0.0.1",
-            //         "shipping_type" => "Sedex",
-            //         "shipping_price" => "12",
-            //         "price_discount" => "",
-            //         "url_notification" => "http://www.loja.com.br/notificacao",
-            //         "free" => "Campo Livre"
-            //     ],
-            //     "transaction_trace" => [
-            //         "estimated_date" => "02/04/2022"
-            //     ],
-            //     "payment" => [
-            //         "payment_method_id" => "3",
-            //         "card_name" => "STEPHEN STRANGE",
-            //         "card_number" => "4111111111111111",
-            //         "card_expdate_month" => "12",
-            //         "card_expdate_year" => "2022",
-            //         "card_cvv" => "644",
-            //         "split" => "1"
-            //     ]
-            // ];
-
             $this->setRules($transaction);
 
             $this->url_environment .= 'transactions/payment';
             $transaction['finger_print'] = $this->finger_print;
             $transaction['token'] = $this->token;
 
-            // $body = Body::json($transaction);
             $body = Body::json($transaction);
 
             $request = new Request($this->url_environment, RequestMethod::POST, [], $body);
@@ -140,10 +76,11 @@ class Transaction implements Parser
             }
 
             $messageErrorResponse = $this->getCodeMessage($codeResponse);
+            $this->message = $messageErrorResponse;
 
-            if (!$messageErrorResponse) {
-                $this->message = $messageErrorResponse;
+            if ($codeResponse === "00") {
                 $bodyResponseParsed = $this->getBodyResponse($bodyResponse);
+
                 $this->tid = $bodyResponse['tid'] ?? -1;
             }
 
@@ -654,10 +591,15 @@ class Transaction implements Parser
         }
     }
 
-    public function paymentMessage($message)
+    public function paymentMessage()
     {
         try {
-            return $this->message;
+            $messageSplited = explode('-', $this->message);
+
+            return [
+                "code" => $messageSplited[0] ?? -1,
+                "message" => $messageSplited[1] ?? ''
+            ];
         } catch (Exception $exception) {
             throw $exception;
         }
@@ -677,6 +619,15 @@ class Transaction implements Parser
         try {
             return $this;
         } catch (Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function refundRaw($tid)
+    {
+        try {
+            return $this->refundByID($tid);
+        } catch (\Exception $exception) {
             throw $exception;
         }
     }
